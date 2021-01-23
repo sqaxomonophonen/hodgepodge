b8e5660cf8f7ab2864231976157d6f2a243d8470
Anders Kaare <anders@straadt.dk>
2021-01-23 10:14:10 +0100

    auto-shading
    based on voxel contour/neighbours

--- codes/isometricfun/isofun.c
+++ codes/isometricfun/isofun.c
@@ -9,10 +9,9 @@
 
 uint32_t palette[] = {
 	0,
-	0xaaaaaa,
-	0xffffff,
-	0xffaaaa,
-	0x0000aa,
+	0x666666,
+	0x999999,
+	0x777777,
 };
 
 static inline uint32_t get_color(int index)
@@ -56,6 +55,21 @@ static inline uint8_t b3d_fetch(struct b3d* b, int x, int y, int z)
 	return b->data[b3d_index(b, x, y, z)];
 }
 
+#define NORMAL_X (1<<0)
+#define NORMAL_Y (1<<1)
+#define NORMAL_Z (1<<2)
+#define CULL     (1<<3)
+
+static inline int b3d_shade(struct b3d* b, int x, int y, int z)
+{
+	if (b3d_fetch(b, x+1, y+1, z+1)) return CULL;
+
+	return
+		  (b3d_fetch(b, x+1, y, z) ? 0 : NORMAL_X)
+		| (b3d_fetch(b, x, y+1, z) ? 0 : NORMAL_Y)
+		| (b3d_fetch(b, x, y, z+1) ? 0 : NORMAL_Z);
+}
+
 static void b3d_plot(struct b3d* b, int x, int y, int z, uint8_t c)
 {
 	if (!b3d_inside(b, x, y, z)) return;
@@ -64,14 +78,55 @@ static void b3d_plot(struct b3d* b, int x, int y, int z, uint8_t c)
 
 #define NCOMP (3)
 
-static inline void plot(uint8_t* pixels, int width, int height, int x, int y, int color_index)
+static inline void plot(uint8_t* pixels, int width, int height, int x, int y, uint32_t color)
 {
 	if (x<0 || x>=width || y<0 || y>=height) return;
-	uint32_t c = get_color(color_index);
 	uint8_t* pixel = &pixels[x*NCOMP + y*NCOMP*width];
-	pixel[0] = (c>>16)&0xff;
-	pixel[1] = (c>>8)&0xff;
-	pixel[2] = (c)&0xff;
+	pixel[0] = (color>>16)&0xff;
+	pixel[1] = (color>>8)&0xff;
+	pixel[2] = (color)&0xff;
+}
+
+static inline int sat8add(int a, int b)
+{
+	int c = a + b;
+	if (c < 0) return 0;
+	if (c > 255) return 255;
+	return c;
+}
+
+static inline uint32_t color_add(uint32_t a, uint32_t b)
+{
+	return sat8add(a&0xff, b&0xff)
+		+ (sat8add((a>>8)&0xff, (b>>8)&0xff) << 8)
+		+ (sat8add((a>>16)&0xff, (b>>16)&0xff) << 16);
+}
+
+static inline uint32_t color_shade(uint32_t color, int flags, int x)
+{
+	int nx = flags & NORMAL_X;
+	int ny = flags & NORMAL_Y;
+	int nz = flags & NORMAL_Z;
+
+	const uint32_t colz = 0x886655;
+	const uint32_t coly = 0x000820;
+	const uint32_t colx = 0x443333;
+
+	if (nz) {
+		color = color_add(color, colz);
+	} else if (nx && !ny) {
+		color = color_add(color, colx);
+	} else if (ny && !nx) {
+		color = color_add(color, coly);
+	} else if (nx && ny) {
+		if (x == 0) {
+			color = color_add(color, coly);
+		} else {
+			color = color_add(color, colx);
+		}
+	}
+
+	return color;
 }
 
 static void b3d_render(struct b3d* b, uint8_t* pixels, int width, int height, int xoffset, int yoffset)
@@ -81,13 +136,16 @@ static void b3d_render(struct b3d* b, uint8_t* pixels, int width, int height, in
 	int sz = b->sz;
 
 	const int xy_scale = 2;
-	const int z_scale = 3;
+	const int z_scale = 2;
 
 	for (int y = 0; y < sy; y++) {
 		for (int z = 0; z < sz; z++) {
 			for (int x = 0; x < sx; x++) {
 				uint8_t v = b3d_fetch(b, x, y, z);
 				if (v == 0) continue;
+
+				int f = b3d_shade(b, x, y, z);
+				if (f & CULL) continue;
 
 				int dx = x*xy_scale - y*xy_scale;
 				int dy = (x+y) - z*z_scale;
@@ -96,7 +154,8 @@ static void b3d_render(struct b3d* b, uint8_t* pixels, int width, int height, in
 					for (int py = 0; py < z_scale; py++) {
 						int tx = dx+px+xoffset;
 						int ty = dy+py+yoffset;
-						plot(pixels, width, height, tx, ty, v);
+
+						plot(pixels, width, height, tx, ty, color_shade(get_color(v), f, px));
 					}
 				}
 			}
@@ -133,7 +192,7 @@ int main(int argc, char** argv)
 
 	for (int y = 0; y < im_height; y++) {
 		for (int x = 0; x < im_width; x++) {
-			plot(pixels, im_width, im_height, x, y, 4);
+			plot(pixels, im_width, im_height, x, y, 0x0000aa);
 		}
 	}
 
