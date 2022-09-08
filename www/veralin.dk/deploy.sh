605e7dc9a9114b5bc61fb9cb1025535481b66a06
Anders Kaare Straadt <anders@straadt.dk>
2022-09-08 22:30:36 +0200

    wikimon

---
+++ www/veralin.dk/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+aws s3 cp --acl public-read --cache-control "max-age=90" index.html s3://veralin.dk/
+aws s3 cp --acl public-read --cache-control "max-age=90" music.html s3://veralin.dk/
+aws s3 cp --acl public-read --cache-control "max-age=90" wikimon.html s3://veralin.dk/
