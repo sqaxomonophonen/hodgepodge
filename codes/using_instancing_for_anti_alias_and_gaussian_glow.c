d7b693068daf1207b723fc0303e693228d9b92e1
Anders Kaare Straadt <anders@straadt.dk>
2023-02-04 21:24:23 +0100

    works

--- codes/using_instancing_for_anti_alias_and_gaussian_glow.c
+++ codes/using_instancing_for_anti_alias_and_gaussian_glow.c
@@ -201,12 +201,12 @@ static struct render* new_render(int multisample_log2)
 		"	vec4 p = u_tx * vec4(u_origin + u_multiplier*a_pos, 0.0, 1.0);\n"
 		"\n"
 		"	uint i = uint(gl_InstanceID);\n"
-		"	vec2 ip = vec2(mod(i, u_samples.x), float(i / u_samples.x)) / vec2(u_samples);\n"
+		"	vec2 ip = vec2(mod(float(i), float(u_samples.x)), float(i / u_samples.x)) / vec2(u_samples);\n"
 		"	vec2 u2 = vec2(2.0, 2.0) / u_dim;\n"
 		"	vec2 d = u2*ip;\n"
 		"\n"
 		"	v_color = a_color;\n"
-		"	gl_Position = p + vec4(d, 0.0, 0.0);\n"
+		"	gl_Position = vec4(p.xyz/p.w,1) + vec4(d, 0.0, 0.0);\n"
 		"}\n"
 	,
 		"#version 140\n"
@@ -284,7 +284,10 @@ static void render_chunk(struct render* render, struct chunk* chunk)
 	const int n_vertex_attribs = 2;
 
 	glBindBuffer(GL_ARRAY_BUFFER, chunk->gl_buffers[0]); CHKGL;
+	glBufferSubData(GL_ARRAY_BUFFER, 0, chunk->n_vertices * sizeof(*chunk->vertices), chunk->vertices); CHKGL;
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, chunk->gl_buffers[1]); CHKGL;
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, chunk->n_indices * sizeof(*chunk->indices), chunk->indices); CHKGL;
 
 	for (int i = 0; i < n_vertex_attribs; i++) {
 		glEnableVertexAttribArray(i); CHKGL;
@@ -334,10 +337,12 @@ int main(int argc, char** argv)
 
 	struct chunk* chunk = new_chunk(gb_vec2(0,0), 100.0);
 	{
-		uint16_t v0 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={0,    0    }, .a_color=0xffff00ff });
-		uint16_t v1 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={1000, 0    }, .a_color=0xffff00ff });
-		uint16_t v2 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={1000, 1000 }, .a_color=0xffff00ff });
-		uint16_t v3 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={0,    1000 }, .a_color=0xffff00ff });
+		const uint32_t c = 0x000f0f0f; // n=2
+		//const uint32_t c = 0x00030303; // n=3
+		uint16_t v0 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={100,  100    }, .a_color=c });
+		uint16_t v1 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={1500, 0    },   .a_color=c });
+		uint16_t v2 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={1000, 1000 },   .a_color=c });
+		uint16_t v3 = chunk_add_vertex(chunk, (struct vertex) { .a_pos={0,    1000 },   .a_color=c });
 		chunk_add_triangle(chunk, v0, v1, v2);
 		chunk_add_triangle(chunk, v0, v2, v3);
 	}
@@ -345,12 +350,16 @@ int main(int argc, char** argv)
 	int exiting = 0;
 	float pitch = 0.0;
 	float yaw = 0.0;
-	gbVec3 campos = gb_vec3(0, 0, 10);
+	gbVec3 campos = gb_vec3(0, 0, -10);
 
 	glEnable(GL_BLEND); CHKGL;
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); CHKGL;
+	glBlendFuncSeparate(
+		GL_ONE, GL_ONE_MINUS_SRC_ALPHA,
+		GL_ONE, GL_ZERO); CHKGL;
 	glDisable(GL_CULL_FACE); CHKGL;
 	glDisable(GL_DEPTH_TEST); CHKGL;
+
+	printf("instance count: %d\n", render_get_instance_count(render));
 
 	while (!exiting) {
 		SDL_Event ev;
@@ -365,7 +374,7 @@ int main(int argc, char** argv)
 		}
 
 		glViewport(0, 0, g.true_screen_width, g.true_screen_height);
-		glClearColor(0,0.2,0,1);
+		glClearColor(0,0,0,1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		render_prep(render, g.true_screen_width, g.true_screen_height, campos, pitch, yaw);
@@ -375,8 +384,8 @@ int main(int argc, char** argv)
 		SDL_GL_SwapWindow(g.window);
 
 
-		pitch += 0.001f;
-		yaw += 0.2f;
+		//pitch += 0.001f;
+		//yaw += 0.1f;
 	}
 
 	SDL_GL_DeleteContext(g.gl_context);
