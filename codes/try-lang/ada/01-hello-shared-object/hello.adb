f37a47235f386ae4879c5e4c06b2a7fdaa6b8323
Anders Kaare Straadt <anders@straadt.dk>
2024-11-08 20:21:37 +0100

    also testing/demonstrating that tasking works

--- codes/try-lang/ada/01-hello-shared-object/hello.adb
+++ codes/try-lang/ada/01-hello-shared-object/hello.adb
@@ -1,7 +1,20 @@
 with Ada.Text_IO;
 package body Hello is
-   procedure Hello_World is
+
+   task type My_Task_Type;
+
+   task body My_Task_Type is
    begin
+      for I in 1..10 loop
+         Ada.Text_IO.Put_Line ("hello from a task!" & Integer'Image(I));
+         delay 0.1;
+      end loop;
+   end My_Task_Type;
+
+   procedure Hello_World is
+      My_Task : access My_Task_Type;
+   begin
+      My_Task := new My_Task_Type;
       Ada.Text_IO.Put_Line ("hellø wørld!");
    end Hello_World;
 end Hello;
