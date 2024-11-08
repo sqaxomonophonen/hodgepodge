e48dfe47828d98757f205fae9feb298c8edfe449
Anders Kaare Straadt <anders@straadt.dk>
2024-11-08 18:27:33 +0100

    ada stuff

---
+++ codes/try-lang/ada/01-hello-shared-object/clean.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -e
+rm -rf b~*.* *.o *.so *.ali *.stdout *.stderr *.lexch test_so
---
+++ codes/try-lang/ada/01-hello-shared-object/hello.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO;
+package body Hello is
+   procedure Hello_World is
+   begin
+      Ada.Text_IO.Put_Line ("hellø wørld!");
+   end Hello_World;
+end Hello;
