ba84aba6192f7570adf9bcded0c49b6ee1deb4bb
Anders Kaare Straadt <anders@straadt.dk>
2024-12-01 01:16:33 +0100

    stuff/credits

--- www/veralin.dk/deploy.sh
+++ www/veralin.dk/deploy.sh
@@ -2,3 +2,4 @@
 aws s3 cp --acl public-read --cache-control "max-age=90" index.html s3://veralin.dk/
 aws s3 cp --acl public-read --cache-control "max-age=90" music.html s3://veralin.dk/
 aws s3 cp --acl public-read --cache-control "max-age=90" wikimon.html s3://veralin.dk/
+aws s3 cp --acl public-read --cache-control "max-age=90" ../../codes/gpujam.html s3://veralin.dk/
