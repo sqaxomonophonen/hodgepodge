9d002324fea1d65fe830dcf7fdb969ff801e225b
Anders Kaare Straadt <anders@straadt.dk>
2024-12-07 14:14:26 +0100

    fakehdr

--- www/veralin.dk/deploy.sh
+++ www/veralin.dk/deploy.sh
@@ -3,3 +3,4 @@ aws s3 cp --acl public-read --cache-control "max-age=90" index.html s3://veralin
 aws s3 cp --acl public-read --cache-control "max-age=90" music.html s3://veralin.dk/
 aws s3 cp --acl public-read --cache-control "max-age=90" wikimon.html s3://veralin.dk/
 aws s3 cp --acl public-read --cache-control "max-age=90" ../../codes/gpujam.html s3://veralin.dk/
+aws s3 cp --acl public-read --cache-control "max-age=90" ../../codes/fakehdr.html s3://veralin.dk/
